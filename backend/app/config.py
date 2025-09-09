f29b7277fcbc5171a150ee473fb3434280c809fb
VEER DODIYA <dodiyaveer12@gmail.com>
2025-09-09 18:59:27 +0530

    Refactor config settings to provide default values for MONGO_URI and DB_NAME; remove bcrypt from requirements.txt

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,8 +2,8 @@ from pydantic_settings import BaseSettings
 import os
 
 class Settings(BaseSettings):
-    MONGO_URI: str
-    DB_NAME: str
+    MONGO_URI: str = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+    DB_NAME: str = os.getenv("DB_NAME", "mydatabase")
     API_KEY: str = os.getenv("API_KEY")
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     google_client_id: str = os.getenv("GOOGLE_CLIENT_ID")
